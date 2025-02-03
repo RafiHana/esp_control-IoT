02eadadf8fed8bb222d5281b3df44360c07b3820
RafiHana <rhntzy29@gmail.com>
2025-02-04 03:04:17 +0700

    update code

--- esp_control.ino
+++ esp_control.ino
@@ -1,81 +1,70 @@
 #include <WiFi.h>
-#include <PubSubClient.h>
+#include <FirebaseESP32.h>
 
-const char* ssid = "NAMA_WIFI";
-const char* password = "PASSWORD_WIFI";
+#define WIFI_SSID " "
+#define WIFI_PASSWORD " "
+#define FIREBASE_HOST " "
+#define FIREBASE_AUTH " "
 
-const char* mqttBroker = "broker.emqx.io"; 
-const int mqttPort = 1883;
-const char* clientId = "esp32-client";
-const char* temperatureTopic = "suhu";
-const char* humidityTopic = "kelembapan";
-const char* co2Topic = "karbon_dioksida";
-const char* smokeTopic = "asap";
-
-WiFiClient espClient;
-PubSubClient mqttClient(espClient);
+FirebaseData firebaseData;
 
 void setup() {
   Serial.begin(115200);
-  connectWiFi();
-  mqttClient.setServer(mqttBroker, mqttPort);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi");
+  }
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
 }
 
 void loop() {
-  if (!mqttClient.connected()) {
-    connectMQTT();
+  float temperature = readTemperature();
+  float humidity = readHumidity();
+  float co2 = readCO2();
+  float smoke = readSmoke();
+
+  if (Firebase.setFloat(firebaseData, "/suhu", temperature)) {
+    Serial.println("Succes");
+  } else {
+    Serial.println("Failed");
   }
-  mqttClient.loop();
 
-  float temperature = readTemperature(); 
-  float humidity = readHumidity();       
-  int co2 = readCO2();                 
-  int smoke = readSmoke();              
+  if (Firebase.setFloat(firebaseData, "/kelembapan", humidity)) {
+    Serial.println("Succes");
+  } else {
+    Serial.println("Failed");
+  }
 
-  mqttClient.publish(temperatureTopic, String(temperature).c_str());
-  mqttClient.publish(humidityTopic, String(humidity).c_str());
-  mqttClient.publish(co2Topic, String(co2).c_str());
-  mqttClient.publish(smokeTopic, String(smoke).c_str());
+  if (Firebase.setFloat(firebaseData, "/karbon_dioksida", co2)) {
+    Serial.println("Succes");
+  } else {
+    Serial.println("Failed");
+  }
+
+  if (Firebase.setFloat(firebaseData, "/asap", smoke)) {
+    Serial.println("Succes");
+  } else {
+    Serial.println("Failed");
+  }
 
   delay(5000); 
 }
 
 float readTemperature() {
-  return 30.5;
+  return 25.0; 
 }
 
 float readHumidity() {
-  return 60.0;
+  return 60.0; 
 }
 
-int readCO2() {
-  return 500;
+float readCO2() {
+  return 400.0; 
 }
 
-int readSmoke() {
-  return 200;
-}
-
-void connectWiFi() {
-  WiFi.begin(ssid, password);
-  Serial.print("Menghubungkan ke WiFi...");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(".");
-  }
-  Serial.println("\nTerhubung ke WiFi!");
-}
-
-void connectMQTT() {
-  while (!mqttClient.connected()) {
-    Serial.print("Menghubungkan ke MQTT...");
-    if (mqttClient.connect(clientId)) {
-      Serial.println("Terhubung ke MQTT!");
-    } else {
-      Serial.print("Gagal, error code: ");
-      Serial.print(mqttClient.state());
-      Serial.println(" Coba lagi dalam 2 detik...");
-      delay(2000);
-    }
-  }
+float readSmoke() {
+  return 0.0; 
 }
